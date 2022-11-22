f596b554524505a6faf6b7b3f03c86d96d6aa79f
haloandfable <tylerallen@live.com>
2022-11-22 13:03:37 -0500

    test

--- src/pages/Outcome.js
+++ src/pages/Outcome.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Spinner from 'react-bootstrap/spinner';
+// import Spinner from 'react-bootstrap/spinner';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
